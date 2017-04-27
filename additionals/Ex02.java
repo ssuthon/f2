0ebcad64dcb2aa6d2e7ff34c18fd567a2295ff1e
ssuthon <suthon@gmail.com>
2017-04-27 11:48:23 +0700

    Update Ex02.java

--- additionals/Ex02.java
+++ additionals/Ex02.java
@@ -1,7 +1,7 @@
 class Ex02{
 	public static void main(String[] args){
 		//Java Exception has 2 main types
-		// 1) Unchecked Exception
+		// 1) Unchecked Exception: Exceptions that extends RuntimeException
 		// 2) Checked Exception
 		try{
 			Thread.sleep(2000);
